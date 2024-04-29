ac4aaf1c0cc85b8c18da5531e2712384e74104cb
wipdev-tech <wipdev.tech@gmail.com>
2024-04-30 01:18:35 +0300

    Handle brace matching

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 )
 
 type TokenType int
+
+type Token struct {
+	Type  TokenType
+	Value string
+}
 
 const (
 	BeginObject    TokenType = iota // Opening curly brace `{` token
@@ -33,10 +39,59 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer f.Close()
+
+	err = isValid(tokenize(f))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Valid JSON")
+}
+
+func tokenize(f *os.File) []Token {
+	TokenMap := map[string]TokenType{
+		"{":     BeginObject,
+		"}":     EndObject,
+		"[":     BeginArray,
+		"]":     EndArray,
+		":":     NameSeparator,
+		",":     ValueSeparator,
+		"true":  True,
+		"false": False,
+		"null":  Null,
+	}
 
 	s := bufio.NewScanner(f)
 	s.Split(bufio.ScanRunes)
+
+	tokens := []Token{}
 	for s.Scan() {
-		fmt.Println(s.Text())
+		c := s.Text()
+		if tokenType, ok := TokenMap[s.Text()]; ok {
+			tokens = append(tokens, Token{Type: tokenType, Value: c})
+		}
 	}
+
+	return tokens
+}
+
+func isValid(tokens []Token) error {
+	nBeginObject := 0
+	nEndObject := 0
+
+	for _, t := range tokens {
+		switch t.Type {
+		case BeginObject:
+			nBeginObject++
+		case EndObject:
+			if nBeginObject == 0 {
+				return errors.New("unexpected closing brace")
+			}
+			nEndObject++
+		}
+	}
+
+	return nil
 }
