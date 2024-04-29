af394f06ed3d4387b55a21401244c7675be37d6e
wipdev-tech <wipdev.tech@gmail.com>
2024-04-29 16:36:50 +0300

    Add test files and token constants
    This is just an inital representation for tokens.

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Token int
+
+const (
+	OpenBrace    Token = iota // Opening curly brace `{` token
+	CloseBrace                // Closing curly brace `}` token
+	OpenBracket               // Opening square bracket `[` token
+	CloseBracket              // Closing square bracket `]` token
+	True                      // Boolean `true` token
+	False                     // Boolean `false` token
+	Null                      // `null` token
+)
 
 func main() {
-	fmt.Println("vim-go")
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanRunes)
+	for s.Scan() {
+		fmt.Println(s.Text())
+	}
 }
